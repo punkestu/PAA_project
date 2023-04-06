5398b405d2182f329f8ea048892021e1b7f807d8
punkestu <pangestubima89@gmail.com>
2023-04-06 16:34:39 +0700

    add menu controller + getter

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const authApi = require("./api/auth");
 const favoriteApi = require("./api/favorite");
+const menuApi = require("./api/menu");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,5 +16,6 @@ app.use(cors());
 // route
 app.use("/auth", authApi);
 app.use("/favorite", favoriteApi);
+app.use("/menu", menuApi);
 
 app.listen(3001, ()=>console.log("listening at http://localhost:3001"));
