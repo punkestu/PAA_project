ba394a9e35a77b2c43066b783d744c84ddb7d6d0
punkestu <pangestubima89@gmail.com>
2023-04-06 16:13:00 +0700

    select menu on favorite getter

--- controller/favorite.js
+++ controller/favorite.js
@@ -1,64 +1,64 @@
-const {favorite} = require("./db");
+const { favorite } = require("./db");
 
 module.exports = {
-    get: async function (req, res) {
-        try {
-            const userId = req.User.Id;
-            const Favorites = await favorite.findMany({
-                where: {
-                    user: {
-                        Id: userId
-                    }
-                },
-                include: {
-                    menu: true
-                }
-            });
-            res.status(200).send({data: Favorites});
-        } catch (e) {
-            res.status(500).send({errors: e});
-        }
-    },
-    add: async function (req, res) {
-        try {
-            const userId = req.User.Id;
-            const menuId = parseInt(req.params.menuId);
-            const Favorite = await favorite.create({
-                data: {
-                    menu: {
-                        connect: {
-                            Id: menuId
-                        }
-                    },
-                    user: {
-                        connect: {
-                            Id: userId
-                        }
-                    }
-                }
-            });
-
-            res.status(200).send({result: Favorite});
-        } catch (e) {
-            console.log(e);
-            res.status(500).send({errors: e});
-        }
-    },
-    delete: async function (req, res) {
-        try{
-            const userId = req.User.Id;
-            const menuId = parseInt(req.params.menuId);
-            const Favorite = await favorite.delete({
-                where: {
-                    menu_id_user_id: {
-                        menu_id: menuId,
-                        user_id: userId
-                    }
-                }
-            })
-            res.status(200).send({result: Favorite});
-        }catch (e) {
-            res.status(500).send({errors: e});
-        }
+  get: async function (req, res) {
+    try {
+      const userId = req.User.Id;
+      const Favorites = await favorite.findMany({
+        where: {
+          user: {
+            Id: userId,
+          },
+        },
+        select: {
+          menu: true
+        },
+      });
+      res.status(200).send({ data: Favorites });
+    } catch (e) {
+      res.status(500).send({ errors: e });
     }
+  },
+  add: async function (req, res) {
+    try {
+      const userId = req.User.Id;
+      const menuId = parseInt(req.params.menuId);
+      const Favorite = await favorite.create({
+        data: {
+          menu: {
+            connect: {
+              Id: menuId,
+            },
+          },
+          user: {
+            connect: {
+              Id: userId,
+            },
+          },
+        },
+      });
+
+      res.status(200).send({ result: Favorite });
+    } catch (e) {
+      console.log(e);
+      res.status(500).send({ errors: e });
+    }
+  },
+  delete: async function (req, res) {
+    try {
+      const userId = req.User.Id;
+      const menuId = parseInt(req.params.menuId);
+      const Favorite = await favorite.delete({
+        where: {
+          menu_id_user_id: {
+            menu_id: menuId,
+            user_id: userId,
+          },
+        },
+      });
+      res.status(200).send({ result: Favorite });
+    } catch (e) {
+      res.status(500).send({ errors: e });
+    }
+  },
 };
